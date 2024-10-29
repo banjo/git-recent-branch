4b7393b15307faf53ee430562d45596a9c36f8ed
Anton Ödman <anton.odman@gmail.com>
2024-10-29 13:29:35 +0100

    chore: first v1 change

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { cancel, intro, isCancel, outro, select } from "@clack/prompts";
+import { select } from "@inquirer/prompts";
 import { execa } from "execa";
 import minimist from "minimist";
 import pc from "picocolors";
@@ -43,7 +43,7 @@ const main = async () => {
         branches = stdout;
     } catch (error: any) {
         if (error.stderr.includes("not a git repository")) {
-            outro("This is not a git repository 🤷");
+            console.log("This is not a git repository 🤷");
             process.exit(0);
         } else {
             console.log(error.stderr);
@@ -53,9 +53,10 @@ const main = async () => {
 
     const current = await currentBranch();
 
-    intro(`Checkout one of your most recent branches`);
+    console.log(pc.bgCyan(pc.black(" Git recent branch ".toUpperCase())));
+    console.log();
 
-    let options = [];
+    let choices = [];
     for (let branch of branches.split("\n")) {
         branch = branch.replace(/'/g, "");
         const time = new RegExp(/\(([^)]+)\)/).exec(branch)?.[1];
@@ -65,33 +66,28 @@ const main = async () => {
             continue;
         }
 
-        options.push({
+        choices.push({
             value: name,
-            label: `${pc.green(name)} - ${time}`,
-            hint: `${hash}`,
+            name: `${pc.green(name)} (${hash}) - ${time}`,
         });
     }
 
     const branch = await select({
         message: "Select the branch you want to checkout",
-        options,
+        choices,
+    }).catch(() => {
+        console.log(pc.red("\n❌ No branch selected"));
+        process.exit(0);
     });
 
-    if (isCancel(branch)) {
-        cancel("No branch selected.");
-        process.exit(0);
-    }
+    console.log(`➡️  Checking out ${pc.green(branch)}!\n`);
 
     try {
-        await execa("git", ["checkout", branch], { stdio: "ignore" });
+        await execa("git", ["checkout", branch], { stdio: "inherit" });
     } catch (error: any) {
-        outro(`Could not checkout branch: ${branch} ❌`);
-
-        console.log(error.stderr);
+        console.log(`\n❌ Could not checkout branch: ${pc.green(branch)}`);
         process.exit(0);
     }
-
-    outro(`Branch ${pc.green(branch)} checked out! ✅`);
-}
+};
 
 main().catch(console.error);
